3e80c7493af43c0662cec7f22f07b58403b8b2a9
Christopher Toth <q.alpha@gmail.com>
2021-09-02 01:21:13 -0600

    Added MOO decompile support to decompile.js to more easily compare trees.

--- sawmill/decompile.js
+++ sawmill/decompile.js
@@ -2,6 +2,8 @@
 const acorn = require("acorn");
 const readline = require("readline");
 const util = require("util");
+const mooparser = require("./src/mooparser").get_parser();
+let decompiler;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,6 +14,19 @@ const rl = readline.createInterface({
 rl.prompt();
 
 rl.on("line", (line) => {
+  const command = line.trim();
+  if (command === "exit") {
+    rl.close();
+  } else if (command === "help") {
+    console.log("Commands: exit, help, javascript, moo");
+    return rl.prompt();
+  } else if (command === "javascript") {
+    decompiler = decompileJavascript;
+    return rl.prompt();
+  } else if (command === "moo") {
+    decompiler = decompileMoo;
+    return rl.prompt();
+  }
   try {
     displayTree(decompile(line));
   } catch (e) {
@@ -21,9 +36,19 @@ rl.on("line", (line) => {
 });
 
 function decompile(data) {
+  return decompiler(data);
+}
+
+function decompileJavascript(data) {
   return acorn.parse(data, { ecmaVersion: 2021, sourceType: "module" });
+}
+
+function decompileMoo(data) {
+  return mooparser.parse(data);
 }
 
 function displayTree(tree) {
   return console.dir(tree, { depth: null, maxArrayLength: null });
 }
+
+decompiler = decompileJavascript;
